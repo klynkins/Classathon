9aca4376a968a2041eb000cbac3103a1d0a94878
Katelynn Campbell <klyncamp23@gmail.com>
2018-04-19 00:26:42 +0000

    sumPrimes

---
+++ fibonacci/fibonacci.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int sumFibs(void);
+
+int main(void)
+{
+    int n = get_int("Sum of Fibonacci numbers is : ");
+    printf = ("%i", n)
+}
+
+int sumFibs(void)
+{
+    int a = 0;
+    int b = 1;
+    int count = 0;
+
+    while(b <= num)
+    {
+    [b, a] = [b+a, b]
+    if(a%2 !==0)
+    {
+      count+=a
+    }
+    }
+    return count;
+}
+
