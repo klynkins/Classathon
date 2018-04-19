728e50897d1899b12646795b7eb4968fef295bbd
Katelynn Campbell <klyncamp23@gmail.com>
2018-04-19 21:54:37 +0000

    classathon

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,28 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int sumFibs(void);
+int sumFibs(int num);
 
-int main(void)
-{
-    int n = get_int("Sum of Fibonacci numbers is : ");
-    printf = ("%i", n)
+int main(void) {
+    int num = get_int("Enter a number: ");
+    printf("%i\n", sumFibs(num));
 }
 
-int sumFibs(void)
-{
-    int a = 0;
-    int b = 1;
-    int count = 0;
+int sumFibs(int num) {
+    int previousNumber = 0;
+    int currentNumber = 1;
+    int result = 0;
 
-    while(b <= num)
-    {
-    [b, a] = [b+a, b]
-    if(a%2 !==0)
-    {
-      count+=a
+    while(currentNumber <= num) {
+        if(currentNumber % 2 != 0) {
+            result += currentNumber;
+        }
+
+        currentNumber += previousNumber;
+        previousNumber = currentNumber - previousNumber;
     }
-    }
-    return count;
+    return result;
 }
 
